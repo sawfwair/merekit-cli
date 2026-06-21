58f98f8c918ea8515db9fd10c6d9160722571f22
Kyle McCullough <director@thebarc.ca>
2026-06-21 16:58:49 -0300

    Use shared Mere docs theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,14 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import { createMereProductDocsTheme } from '@mere/docs-theme'
 
-export default {
-  extends: DefaultTheme
-}
+export default createMereProductDocsTheme({
+  productName: 'Mere CLI',
+  productDomain: 'cli-docs.merekit.com',
+  docsUrl: 'https://cli-docs.merekit.com/',
+  productHref: 'https://github.com/sawfwair/merekit-cli',
+  corePrefix: 'The user and agent command plane for Mere workspaces.',
+  guideHref: '/getting-started',
+  architectureHref: '/business-site',
+  operationsHref: '/ops',
+  referenceHref: '/commands',
+  cliHref: '/commands'
+})
